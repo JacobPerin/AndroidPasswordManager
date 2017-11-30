4881cde791df0a4d084e2aafa4f40dbdbcac53d4
mcelvain <mcelvain@iastate.edu>
2017-11-29 21:31:50 -0600

    Fixed Merge

--- app/src/main/java/com/example/jacobgperin/androidpasswordmanager/model/PasswordDataSource.java
+++ app/src/main/java/com/example/jacobgperin/androidpasswordmanager/model/PasswordDataSource.java
@@ -1,11 +1,18 @@
 package com.example.jacobgperin.androidpasswordmanager.model;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.jacobgperin.androidpasswordmanager.database.PasswordBaseHelper;
+import com.example.jacobgperin.androidpasswordmanager.database.PasswordDBSchema;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static com.example.jacobgperin.androidpasswordmanager.database.PasswordDBSchema.*;
 
 /**
  * Created by Jacob G. Perin on 11/28/2017.
@@ -15,6 +22,8 @@ public class PasswordDataSource {
     private static PasswordDataSource mPasswordDataSource;
 
     private List<Password> mPasswords;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static PasswordDataSource get(Context context){
         if(mPasswordDataSource == null){
@@ -25,26 +34,26 @@ public class PasswordDataSource {
     }
 
     private PasswordDataSource(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new PasswordBaseHelper(mContext).getWritableDatabase();
         mPasswords = new ArrayList<>();
 
         // FIXME :: filled w/ dummy data until database is set up IN this file (Chapter 14)
 
-        for(int i = 0; i < 20; i++){
-            Password password = new Password();
-
-            // Generate some tags
-            ArrayList<String> tags = new ArrayList<>();
-
-            for(int j = 0; j < 5; j++){
-                tags.add(randomString(5));
-            }
-
-            // Set w/ dummy data
-            password.setmTags(tags);
-            password.setmPassword(randomString(10));
-
-            mPasswords.add(password);
-        }
+//        for(int i = 0; i < 20; i++){
+//            Password password = new Password();
+//
+//            // Generate some tags
+//            ArrayList<String> tags = new ArrayList<>();
+//
+//            for(int j = 0; j < 5; j++){
+//                tags.add(randomString(5));
+//            }
+//
+//            // Set w/ dummy data
+//            password.setmTags(tags);
+//            password.setmPassword(randomString(10));
+//        }
 
     }
 
@@ -70,6 +79,28 @@ public class PasswordDataSource {
         return mPasswords;
     }
 
+    public void addPassword(Password password){
+        ContentValues values = getPasswordContentValues(password);
+        mDatabase.insert(PasswordTable.NAME, null, values);
+
+        values.clear();
+        for(int i = 0; i < password.getmTags().size(); i++){
+            values = getTagContentValues(password.getmTags().get(i), password.getId().toString());
+            mDatabase.insert(TagTable.NAME, null, values);
+        }
+    }
+
+    public void updatePassword(Password password){
+        String id = password.getId().toString();
+        ContentValues values = getPasswordContentValues(password);
+
+        mDatabase.update(PasswordTable.NAME, values,
+                PasswordTable.Columns.UUID + " = ?",
+                new String[] {id});
+
+        values.clear();
+    }
+
     public Password getPassword(UUID id) {
         for(Password password : mPasswords) {
             if(password.getId().equals(id)){
@@ -79,4 +110,21 @@ public class PasswordDataSource {
         return null;
     }
 
+    private static ContentValues getPasswordContentValues(Password password){
+        ContentValues values = new ContentValues();
+        values.put(PasswordTable.Columns.UUID, password.getId().toString());
+        values.put(PasswordTable.Columns.PASSWORD, password.getmPassword());
+
+        return values;
+    }
+
+    private static ContentValues getTagContentValues(PasswordTag tag, String id){
+        ContentValues values = new ContentValues();
+
+        values.put(TagTable.Columns.UUID, tag.getId().toString());
+        values.put(TagTable.Columns.TAG, tag.getName());
+        values.put(TagTable.Columns.TAGID, id);
+
+        return values;
+    }
 }
